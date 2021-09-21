fff0b188b4b8c7ed1f132dfa684f49b3e2948660
Guillaume TALBOT <talbotgui@gmail.com>
2021-09-21 21:25:30 +0200

    Sauvegarde de décision

--- projects/coupeDesMaisons/src/app/service/dao.ts
+++ projects/coupeDesMaisons/src/app/service/dao.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable, of } from 'rxjs';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { from, Observable, of } from 'rxjs';
 import { map, mergeMap } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 import { Adulte, AnneeScolaire, Bareme, Decision, Groupe } from '../model/model';
@@ -18,11 +18,32 @@ export class Dao {
     private groupes: Observable<Groupe[]> = of([]);
     private baremes: Observable<Bareme[]> = of([]);
 
+    /** Collection firebase des décisions */
+    private firebaseDecisions?: AngularFirestoreCollection<Decision>;
+
     /** Données chargées */
     private annee = new AnneeScolaire();
 
     /** Constructeur pour injection des dépendances */
     constructor(private firestore: AngularFirestore) { }
+
+
+    /** Stockage d'une décision */
+    public ajouterUneDecision(decision: Decision): Observable<boolean> {
+        if (this.firebaseDecisions) {
+            return from(this.firebaseDecisions.add(Object.assign({}, decision))).pipe(map(d => !!d));
+        } else {
+            return of();
+        }
+    }
+
+    /** Génération d'un UUID (@see https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid) */
+    private genererUUID(): string {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
 
     /** Chargement de toutes les données */
     public chargerDonnees(): Observable<AnneeScolaire | undefined> {
@@ -37,8 +58,9 @@ export class Dao {
         if (!this.observablesInitialises) {
             // { idField: 'id' } permet de récupérer l'ID du document Firestore dans l'attribut 'id'
             // @see https://jsmobiledev.com/article/angularfire-idfield/
+            this.firebaseDecisions = this.firestore.collection<Decision>('decisions');
+            this.decisions = this.firebaseDecisions.valueChanges({ idField: 'id' });
             this.adultes = this.firestore.collection<Adulte>('adultes').valueChanges({ idField: 'id' });
-            this.decisions = this.firestore.collection<Decision>('decisions').valueChanges({ idField: 'id' });
             this.groupes = this.firestore.collection<Groupe>('groupes').valueChanges({ idField: 'id' });
             this.baremes = this.firestore.collection<Bareme>('baremes').valueChanges({ idField: 'id' });
             this.observablesInitialises = true;
