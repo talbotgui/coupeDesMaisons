63f97d7b69a1835a4ecd7e89d7d56fdc58ad8cc8
Guillaume TALBOT <guillaume.talbot@cgi.com>
2021-09-28 08:59:10 +0200

    Amélioration de la gestion des erreurs

--- projects/coupeDesMaisons/src/app/service/erreur.ts
+++ projects/coupeDesMaisons/src/app/service/erreur.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 
 /**
  * Classe traitant les erreurs.
@@ -6,8 +7,17 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class GestionnaireErreur {
 
+    private static MAT_SNACK_CONFIG = Object.assign(
+        new MatSnackBarConfig(),
+        { politeness: 'polite', duration: 5000, horizontalPosition: 'right', verticalPosition: 'bottom' }
+    );
+
+    /** Constructeur avec injection de dépendances */
+    public constructor(private snackbar: MatSnackBar) { }
+
     /** Affichage d'un message à l'utilisateur en fonction du type d'erreur */
-    public gererMessageDerreur(erreur: object): void {
-        console.error(erreur);
+    public gererMessageDerreur(message: string, erreur: Error): void {
+        this.snackbar.open(message + ' (' + erreur.message + ')', 'Erreur', GestionnaireErreur.MAT_SNACK_CONFIG);
+        console.error(message, erreur);
     }
 }
