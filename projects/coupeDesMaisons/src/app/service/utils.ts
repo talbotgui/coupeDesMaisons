0f08b36c37ace1013c51c9d4abe371487a29b064
Guillaume TALBOT <talbotgui@gmail.com>
2021-10-05 18:05:38 +0200

    Correction de la génération de la date

--- projects/coupeDesMaisons/src/app/service/utils.ts
+++ projects/coupeDesMaisons/src/app/service/utils.ts
@@ -5,7 +5,7 @@ export class Utils {
     /** Créer la date du jour en string formattée */
     public static creerDateFormatee(): string {
         const d = new Date();
-        return (d.getDay() < 10) ? '0' : '' + d.getDay() + '/' + (d.getMonth() < 10) ? '0' : '' + d.getMonth() + '/' + d.getFullYear();
+        return ((d.getDay() < 10) ? '0' : '') + d.getDay() + '/' + ((d.getMonth() < 10) ? '0' : '') + d.getMonth() + '/' + d.getFullYear();
     }
 
 }
